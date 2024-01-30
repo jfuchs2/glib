be79ca2366b3645abf1d4cd4cd6e23be8d6208f8
Thomas Haller <thaller@redhat.com>
2024-01-30 13:09:19 +0100

    gobject: drop OPTIONAL_BIT_LOCK_TOGGLE_REFS lock
    It was replaced by the GData lock and g_datalist_id_update_atomic().
    
    Note that we introduced object_bit_lock() to replace global mutexes.
    Now, object_bit_lock() is itself completely replaced, by the GData lock
    (and hooking it via g_datalist_id_update_atomic()).
    
    This means, all mutex-like locks now only go through the GData lock on
    the GObject's qdata.
    
    The object_bit_lock() API is still here, because it might be useful. For
    example, there might be cases where the GData lock is not sufficient.
    
    Also, we introduced GObjectPrivate mainly for object_bit_lock(). On 32
    bit architecture, there is an overhead with that, as we cannot fit the
    optional_flags into GObject. This is not dropped either. The optional
    flags seem rather useful even without the object_bit_lock(). For
    example, the OPTIONAL_FLAG_EVER_HAD_WEAK_REF flag is an important
    optimization.

--- gobject/gobject.c
+++ gobject/gobject.c
@@ -121,7 +121,7 @@ enum {
  * parallel as possible. The alternative would be to add individual locking
  * integers to GObjectPrivate. But increasing memory usage for more parallelism
  * (per-object!) is not worth it. */
-#define OPTIONAL_BIT_LOCK_TOGGLE_REFS 1
+#define OPTIONAL_BIT_LOCK_UNUSED 1
 
 #if SIZEOF_INT == 4 && GLIB_SIZEOF_VOID_P >= 8
 #define HAVE_OPTIONAL_FLAGS_IN_GOBJECT 1
@@ -457,7 +457,7 @@ weak_ref_data_has (GObject *object, WeakRefData *wrdata, WeakRefData **out_new_w
 static G_THREAD_LOCAL guint _object_bit_is_locked;
 #endif
 
-static void
+G_GNUC_UNUSED static void
 object_bit_lock (GObject *object, guint lock_bit)
 {
 #if defined(G_ENABLE_DEBUG) && defined(G_THREAD_LOCAL)
@@ -472,7 +472,7 @@ object_bit_lock (GObject *object, guint lock_bit)
   g_bit_lock ((gint *) object_get_optional_flags_p (object), _OPTIONAL_BIT_LOCK);
 }
 
-static void
+G_GNUC_UNUSED static void
 object_bit_unlock (GObject *object, guint lock_bit)
 {
 #if defined(G_ENABLE_DEBUG) && defined(G_THREAD_LOCAL)
@@ -3984,8 +3984,6 @@ toggle_refs_check_and_ref (GObject *object,
   *toggle_notify = NULL;
   *toggle_data = NULL;
 
-  object_bit_lock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
-
   result = _g_datalist_id_update_atomic (&object->qdata,
                                          quark_toggle_refs,
                                          toggle_refs_check_and_ref_cb,
@@ -3996,8 +3994,6 @@ toggle_refs_check_and_ref (GObject *object,
                                              .old_ref = old_ref,
                                              .increment = increment,
                                          }));
-
-  object_bit_unlock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
 
   return GPOINTER_TO_INT (result);
 }
@@ -4093,8 +4089,6 @@ g_object_add_toggle_ref (GObject *object,
 
   g_object_ref (object);
 
-  object_bit_lock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
-
   _g_datalist_id_update_atomic (&object->qdata,
                                 quark_toggle_refs,
                                 toggle_refs_ref_cb,
@@ -4102,8 +4096,6 @@ g_object_add_toggle_ref (GObject *object,
                                     .notify = notify,
                                     .data = data,
                                 }));
-
-  object_bit_unlock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
 }
 
 static gpointer
@@ -4171,8 +4163,6 @@ g_object_remove_toggle_ref (GObject *object,
   g_return_if_fail (G_IS_OBJECT (object));
   g_return_if_fail (notify != NULL);
 
-  object_bit_lock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
-
   result = _g_datalist_id_update_atomic (&object->qdata,
                                          quark_toggle_refs,
                                          toggle_refs_unref_cb,
@@ -4180,8 +4170,6 @@ g_object_remove_toggle_ref (GObject *object,
                                              .notify = notify,
                                              .data = data,
                                          }));
-
-  object_bit_unlock (object, OPTIONAL_BIT_LOCK_TOGGLE_REFS);
 
   if (!GPOINTER_TO_INT (result))
     {
